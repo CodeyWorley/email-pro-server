9c60160bfa53eb917ed4a928935f998dcd12e170
CodeyWorley <codeyworley@gmail.com>
2019-03-19 12:09:47 -0700

    fixed cors

--- server.js
+++ server.js
@@ -12,29 +12,23 @@ const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
 
 mongoose.Promise = global.Promise;
 
-const { PORT, DATABASE_URL, CLIENT_ORIGIN } = require('./config');
+const { PORT, DATABASE_URL } = require('./config');
 
 const app = express();
 
 app.use(morgan('common'));
 
 // CORS
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-//   if (req.method === 'OPTIONS') {
-//     return res.send(204);
-//   }
-//   next();
-// });
-
-// cors
-app.use(
-  cors({
-    origin: CLIENT_ORIGIN
-  })
-);
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+app.use(express.json());
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
@@ -44,21 +38,20 @@ app.use('/api/addresses', addressesRouter);
 app.use('/api/users/', usersRouter);
 app.use('/api/auth/', authRouter);
 
-app.use('*', (req, res) => {
-  return res.status(404).json({ message: 'Not Found' });
-});
-
 let server;
+
 function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl, err => {
+    mongoose.connect(databaseUrl, { useNewUrlParser: true }, err => {
       if (err) {
         return reject(err);
       }
-      server = app.listen(port, () => {
-        console.log(`Your app is listening on port ${port}`);
-        resolve();
-      })
+
+      server = app
+        .listen(port, () => {
+          console.log(`App is listening on port ${port} ${databaseUrl} `);
+          resolve();
+        })
         .on('error', err => {
           mongoose.disconnect();
           reject(err);
